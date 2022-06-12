e81f6cf0e9d7b275456c15e7308e2f9b6a443c32
Pancho Villa <94568874+El-Mariachi760@users.noreply.github.com>
2022-06-12 14:17:09 -0700

    Update Port

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+
 const express = require('express');
 
 const { animals } = require('./data/animals')
 
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // fitering by querry
@@ -50,6 +53,6 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-});
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
